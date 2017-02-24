4538c52c072d1ba5f19569671085c4c7250bea70
Khairul Azmi <khairulfs21@gmail.com>
2017-02-24 10:25:00 +0800

    8 learn about controllers

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,10 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Task;
 
-use Illuminate\Http\Request;
+class TaskController extends Controller {
+	public function index() {
+		//$tasks = DB::table('tasks')->get();
+		$tasks = Task::all();
+		return view('about', compact('tasks'));
+		/*$tasks = [
+			'pergi kerja di netbox',
+			'jangan lupa untuk melabur saham',
+			'ahad ni tak jadi pergi ke langkawi'];
 
-class TaskController extends Controller
-{
-    //
+		return view('about', compact('tasks'));*/
+		/*$name = "khairul azmi";
+		return view('about', compact('name'));*/
+		//return view('about', ['name' => 'khairul']);
+	}
+	public function show($id) {
+		//{id} - wildcards
+		//$task = DB::table('tasks')->find($id);
+		$task = Task::find($id);
+		return view('details', compact('task'));
+
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,5 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
-Route::get('/about', function () {
-
-	//$tasks = DB::table('tasks')->get();
-	$tasks = App\Task::all();
-	return view('about', compact('tasks'));
-	/*$tasks = [
-		'pergi kerja di netbox',
-		'jangan lupa untuk melabur saham',
-		'ahad ni tak jadi pergi ke langkawi'];
-
-	return view('about', compact('tasks'));*/
-	/*$name = "khairul azmi";
-	return view('about', compact('name'));*/
-	//return view('about', ['name' => 'khairul']);
-});
-
-Route::get('/about/{id}', function ($id) {
-	//{id} - wildcards
-
-	//$task = DB::table('tasks')->find($id);
-	$task = App\Task::find($id);
-
-	return view('details', compact('task'));
-
-});
+Route::get('/about', 'TaskController@index');
+Route::get('/about/{id}', 'TaskController@show');
